d847a3e0ad05e4a9d41ae3dd5c2c9ac405e1408d
Cosmos <122612856+CosmosElement77@users.noreply.github.com>
2024-03-06 10:06:41 +0530

    Uploaded changes

--- index.js
+++ index.js
@@ -1,70 +1,42 @@
-// function get_data()
-// {
-//     const arl="https://api.ipify.org?format=json";
-//     fetch(arl ,{method:"GET"});
-
-// 	try{
-// 		const res= fetch(arl);
-// 		const result=res.text();
-// 		console.log(result);
-// 	}
-// 	catch (error){console.log("error found");}
-// }
-
-
-async function  get_media()
+function get_ip()
 {
+    const arl="https://api.ipify.org?format=json";
+    const options={method:"GET"};
 
-const url = 'https://instagram28.p.rapidapi.com/medias?user_id=25025320&batch_size=20';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ef87885f91msh9eb35ef4580f52fp1f6723jsn90a46f556136',
-		'X-RapidAPI-Host': 'instagram28.p.rapidapi.com'
+	try{
+		const res= fetch(arl,options);
+		const result=res.text();
+		console.log(result);
 	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+	catch (error){console.log("error found");}
 }
-}
-let ele=document.getElementById("searchbar");
-let username=localStorage.setItem("ele");
 
-const url = `https://instagram28.p.rapidapi.com/search?search=+${username}`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ef87885f91msh9eb35ef4580f52fp1f6723jsn90a46f556136',
-		'X-RapidAPI-Host': 'instagram28.p.rapidapi.com'
+
+async function get_data()
+{	
+	let username=document.getElementById("searchbar").value;
+	const url = `https://instagram28.p.rapidapi.com/user_info?user_name=${username}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'ef87885f91msh9eb35ef4580f52fp1f6723jsn90a46f556136',
+			'X-RapidAPI-Host': 'instagram28.p.rapidapi.com'
+		}
+	};
+	
+	try {
+		var response =  fetch(url, options);
+		var result =  response.json();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
 	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
 }
 
-// const url = "http://api.football-data.org/v2/matches";
-// fetch(url, {
-//   method: "GET",
-//   withCredentials: true,
-//   headers: {
-//     "X-Auth-Token": "ef72570ff371408f9668e414353b7b2e",
-//     "Content-Type": "application/json"
-//   }
-// })
-//   .then(resp => resp.json())
-//   .then(function(data) {
-//     console.log(data);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
+
+////data.user.biography
+////Following: data.user.edge_follow.count
+////Followers: data.user.edge_followed_by.count
+////Full_name: data.user.full_name
+////Profile Pic: data.user.profile_pic_url_hd
+////If the user is verified or not: data.user.is_verified
